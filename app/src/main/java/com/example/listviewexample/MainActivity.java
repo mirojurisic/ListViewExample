0ce6b67aa38a49302ed8ac3bbc8db276c07f33d9
Jarvis <jarvis.h.yang@outlook.com>
2019-11-14 15:58:23 -0900

    Update MainActivity.java

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -13,27 +13,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
-
+    ArrayList<Player> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView = findViewById(R.id.listview);
-        ArrayAdapter<Player> MyAdapter = new ArrayAdapter<Player>(this, R.layout.activity_main, getPlayers());
-        listView.setAdapter(MyAdapter);
+        getPlayers();
+        MyAdapter myAdapter = new MyAdapter(this, 0, list);
+        listView.setAdapter(myAdapter);
     }
 
-    public List<Player> getPlayers(){
+    public void getPlayers(){
         String sport[] = new String[] {"golf", "tennis", "badminton", "soccer", "football", "basketball", "competitive gaming", "hockey", "synchronized swimming", "track", "cross country", "gymnastics", "ice hockey", "figure skating", "volleyball"};
         String name[] = new String[] {"Mr. Miro", "Alan", "David", "Alex", "Jimmy", "Sunny", "Jesse", "Ronal", "Colin", "Jarvis", "Cindy", "Cathy", "Hedwig", "Quinn", "Philip"};
         int imageResource[] = new int[] {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j, R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n, R.drawable.o};
         int worth[] = new int[] {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         int age[] = new int[] {50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36};
-        ArrayList<Player> list = new ArrayList<Player>();
+             list   = new ArrayList<Player>();
         for(int i = 0; i < 15; i++){
-            list.add(new Player(name[i],1,1.0, sport[i], imageResource[i]));
+            list.add(new Player(name[i],age[i],worth[i], sport[i], imageResource[i]));
         }
-        return list;
+
     }
 
 }
