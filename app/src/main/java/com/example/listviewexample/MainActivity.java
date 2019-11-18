e9e7755fbe3b79a3979e92152337261634498ae3
mgrddsj <Jesse_Xu@live.com>
2019-11-18 08:51:35 +0800

    Add things to list.

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
@@ -18,16 +20,26 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listview);
-        List<Player> list={};
-    //Todo reference listview
-    //  Todo       ListView listView = findViewById();
-    // Todo initialize custom adapter(MyAdapter) - using  method getPlayers, pay attention to constructor of MyAdapter
+        List<Player> list= getPlayers();
+        //Todo reference listview
+        // Todo initialize custom adapter(MyAdapter) - using  method getPlayers, pay attention to constructor of MyAdapter
         MyAdapter adapter=new MyAdapter(this,R.id.listview,list);
-        //Todo  add custom adapter to listview
-        //Todo listView.setAdapter(------);
+        listView.setAdapter(adapter);
+
 
     }
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
-
+    public List getPlayers()
+    {
+        List<Player> list = Arrays.asList(new Player[]{
+                new Player("Doris", 16, 100000, "Basketball", R.drawable.member_placeholder_female),
+                new Player("LeBron James", 34, 40000000, "Basketball", R.drawable.team_man_placeholder),
+                new Player("Kevin Durant", 31, 30000000, "Basketball", R.drawable.team_man_placeholder),
+                new Player("Kyrie Irving", 27, 20100000, "Basketball", R.drawable.team_man_placeholder),
+                new Player("James Harden", 30, 28300000, "Basketball", R.drawable.team_man_placeholder),
+                new Player("Stephen Curry", 31, 37460000, "Basketball", R.drawable.team_man_placeholder)
+        });
+        return list;
+    }
 }
