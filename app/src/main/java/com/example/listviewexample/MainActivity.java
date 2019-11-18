5c6978264bcd7f2f4229001a3f076f4c0fabb2c3
dcindy <2948292626@qq.com>
2019-11-18 09:05:48 +0800

    nrew

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -25,11 +25,20 @@ public class MainActivity extends AppCompatActivity  {
     // Todo initialize custom adapter(MyAdapter) - using  method getPlayers, pay attention to constructor of MyAdapter
         MyAdapter adapter=new MyAdapter(this,R.id.listview,list);
         //Todo  add custom adapter to listview
-        
+        listView.setAdapter(adapter);
         //Todo listView.setAdapter(------);
 
     }
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
+    public List<Player> getPlayers(ArrayList<Player> list)
+    {
+        list.add(new Player("Lionel Messi",32,44224400.00,"football",R.drawable.mesi));
+        list.add(new Player("David Robert Joseph Beckham",44,1291800.00,"football",R.drawable.beckham));
+        list.add(new Player("Hanyu Yuzulu",24,500000,"figure skating",R.drawable.hanyu));
+        list.add(new Player("Zhang Jike",31,500000,"Ping-pong",R.drawable.zhang));
+
+    }
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
+
 
 }
